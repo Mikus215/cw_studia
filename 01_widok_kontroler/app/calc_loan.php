b9d3151429c79a58c1b06142c8c4cf65f5d3976c
Mikus215 <mikimarsz215@gmail.com>
2022-10-18 14:58:04 +0200

    01_widok_kontroler

---
+++ 01_widok_kontroler/app/calc_loan.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once dirname(__FILE__).'/../config.php';
+
+$loan = $_REQUEST ['loan'];
+$paybackPeriod = $_REQUEST ['payback-period'];
+$interesRate = $_REQUEST ['interes-rate'];
+
+if ( ! (is_numeric($loan) && is_numeric($paybackPeriod) && is_numeric($interesRate))) {
+	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+}
+
+if ( !is_numeric( $loan )  ) {
+	$messages [] = 'Nie podano kwoty kredytu';
+}
+if ( !is_numeric( $paybackPeriod )  ) {
+	$messages [] = 'Nie podano okresu spłaty';
+}
+if ( !is_numeric( $interesRate )  ) {
+	$messages [] = 'Nie podano oprocentowania';
+}
+
+if (empty ( $messages )) { 
+	$monthPayment = round((($loan * ($interesRate / 100)) + $loan) / $paybackPeriod, 4);
+	$totalLoanToPay = $monthPayment * $paybackPeriod;
+}
+
+include 'calc_loan_view.php';
---
+++ 01_widok_kontroler/app/calc_loan_view.php
@@ -0,0 +1,48 @@
+<?php require_once dirname(__FILE__) .'/../config.php';?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+<meta charset="utf-8" />
+<title>Kalkulator</title>
+<link rel="stylesheet" href="../style/style.css">
+</head>
+<body>
+
+<form action="<?php print(_APP_URL);?>/app/calc_loan.php" method="post" class="form">
+	<div class="form__input-controller">
+		<label for="load">Kwota kredytu: </label>
+		<input id="load" type="number" name="loan"  class="form__input" require/>
+	</div>
+	<div class="form__input-controller">
+		<label for="payback-period"> Okres spłaty(w miesiącach): </label>
+		<input id="payback-period" type="number" name="payback-period"  class="form__input" require/>
+	</div>
+	<div class="form__input-controller">
+		<label for="interes-rate"> Oprocentowanie(w procentach): </label>
+		<input id="interes-rate" type="number" name="interes-rate"  class="form__input" require max="20"/>
+	</div>
+	<button type="submit" class="form__submit-button">Oblicz</button>
+</form>	
+
+<?php
+
+if (isset($messages)) {
+	if (count ( $messages ) > 0) {
+		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
+		foreach ( $messages as $key => $msg ) {
+			echo '<li>'.$msg.'</li>';
+		}
+		echo '</ol>';
+	}
+}
+?>
+
+<?php if (isset($monthPayment) && isset($totalLoanToPay)){ ?>
+<div style="margin: 20px; padding: 10px; border-radius: 5px; background-color: #ff0; width:300px;">
+<?php echo 'Mięsiecznie: '.$monthPayment; ?> <br />
+<?php echo 'Suma: '.$totalLoanToPay; ?> <br />
+</div>
+<?php } ?>
+
+</body>
+</html>
